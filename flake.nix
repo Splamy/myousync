1b40c4fbeb3cc5508e2f254ae73591f03cea798a
Splamy <splamyn@gmail.com>
2025-11-14 22:32:41 +0100

    x

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     systems = ["x86_64-linux"];
     forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs {inherit system;}));
   in {
-    nixosModules.default = import ./nix/myousync.nix self;
+    nixosModules.myousync = import ./nix/myousync.nix self;
     # overlays.default = import ./nix/overlay.nix;
 
     packages = forAllSystems (pkgs: {
